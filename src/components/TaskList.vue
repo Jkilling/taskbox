46c925d92b39c0524c92c0c7c6006a94afddb9dd
Jkilling <jojok.12@web.de>
2021-04-23 12:21:47 +0200

    finished single component

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,21 +1,52 @@
 <template>
-  <PureTaskList
-    :tasks="tasks"
-    v-on="$listeners"
-    @archive-task="archiveTask"
-    @pin-task="pinTask"
-  />
+  <div class="list-items">
+    <template v-if="loading">
+      <div v-for="n in 6" :key="n" class="loading-item">
+        <span class="glow-checkbox" />
+        <span class="glow-text">
+          <span>Loading</span> <span>cool</span> <span>state</span>
+        </span>
+      </div>
+    </template>
+
+    <div v-else-if="isEmpty" class="list-items">
+      <div class="wrapper-message">
+        <span class="icon-check" />
+        <div class="title-message">You have no tasks</div>
+        <div class="subtitle-message">Sit back and relax</div>
+      </div>
+    </div>
+
+    <template v-else>
+      <Task
+        v-for="task in tasksInOrder"
+        :key="task.id"
+        :task="task"
+        v-on="$listeners"
+      />
+    </template>
+  </div>
 </template>
 
 <script>
-import PureTaskList from "./PureTaskList";
-import { mapState, mapActions } from "vuex";
-
+import Task from "./Task";
 export default {
-  components: { PureTaskList },
-
-  methods: mapActions(["archiveTask", "pinTask"]),
-
-  computed: mapState(["tasks"]),
+  name: "TaskList",
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true, default: () => [] },
+    loading: { type: Boolean, default: false },
+  },
+  computed: {
+    tasksInOrder() {
+      return [
+        ...this.tasks.filter((t) => t.state === "TASK_PINNED"),
+        ...this.tasks.filter((t) => t.state !== "TASK_PINNED"),
+      ];
+    },
+    isEmpty() {
+      return this.tasks.length === 0;
+    },
+  },
 };
 </script>
